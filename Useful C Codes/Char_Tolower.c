183d8bc215f7e911f2964b939ff56c974b492710
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-04-02 15:51:14 +0300

    Enter char and see its lower case

---
+++ Useful C Codes/Char_Tolower.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning (disable:6031)
+#include <stdio.h>
+#include "Omer.h"
+#include "Mytime.h"
+
+int main()
+{
+	int ch;
+	printf("Enter a char: ");
+	ch = getchar();
+	printf("ch = %d\n", ch);
+	printf("%c ---> %c\n", ch, tolower(ch));
+}
