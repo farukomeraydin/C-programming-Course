826847ba87119053740bf9939c43e2998199f15a
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-04-05 13:15:57 +0300

    Prints minimum and maximum element of array

---
+++ Useful_Array_Codes/Min_and_Max.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning (disable:6031)
+#include "Omer.h"
+
+#define SIZE	20
+
+int main()
+{
+	int a[SIZE];
+	randomize();
+	for (int i = 0; i < SIZE; ++i)
+	{
+		a[i] = rand() % 1000;
+		printf("%3d%c", a[i], i % 20 == 19 ? '\n' : ' ');
+	}
+	printf("\n\n");	
+	
+	int min = a[0];
+	int max = a[0];
+	for (int i = 1; i < SIZE; ++i)
+		if (a[i] > max)
+			max = a[i];
+		else if (a[i] < min)
+			min = a[i];
+	printf("min = %d\n", min);
+	printf("max = %d\n", max);
+}
