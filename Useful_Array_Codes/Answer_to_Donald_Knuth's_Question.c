7d59feeb6b19bfeeb9b3a284fb3310a908bb1ea1
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-04-05 07:14:05 +0300

    Donald Knuth's question is answered
    His question is: In linear search algorithm,it takes 3n operations if the number is not found(++i, i<SIZE,and a[i]==val).How can we make it 2n operations?

---
+++ Useful_Array_Codes/Answer_to_Donald_Knuth's_Question.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning (disable:6031)
+#include "Omer.h"
+
+#define SIZE	100
+
+int main()
+{
+	int a[SIZE + 1];
+	randomize();
+	for (int i = 0; i < SIZE; ++i)
+	{
+		a[i] = rand() % 1000;
+		printf("%3d%c", a[i], i % 20 == 19 ? '\n' : ' ');
+	}
+	printf("\n\n");
+	int val;
+	printf("Enter the number you want to search: ");
+	scanf("%d", &val);
+	a[SIZE] = val;
+
+	int i;
+	for (i = 0; a[i] != val; ++i)
+		;
+	if (i < SIZE)
+		printf("Number found index is %d\n", i);
+	else
+		printf("Not found\n");
+	
+}
