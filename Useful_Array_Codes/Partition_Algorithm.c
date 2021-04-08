bb5ff7352d8a91ef1b93709a35736519cd8f3647
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-04-08 22:22:20 +0300

    Odds first, even numbers later.From small to large

---
+++ Useful_Array_Codes/Partition_Algorithm.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning (disable:6031)
+#include "Omer.h"
+
+#define SIZE	100
+
+int main()
+{
+	int a[SIZE];
+	randomize();
+	for (int i = 0; i < SIZE; ++i)
+	{
+		a[i] = rand() % 1000;
+		printf("%3d%c", a[i], i % 20 == 19 ? '\n' : ' ');
+	}
+	printf("\n");
+
+	for (int i = 0; i < SIZE - 1; ++i)
+		for (int k = 0; k < SIZE - 1 - i; ++k)
+			if ((a[k] % 2 == 0 && a[k + 1] % 2 != 0) || (a[k] % 2 == a[k + 1] % 2 && a[k] > a[k + 1])) {
+				int temp = a[k];
+				a[k] = a[k + 1];
+				a[k + 1] = temp;
+			}
+	for (int i = 0; i < SIZE; ++i)
+		printf("%3d%c", a[i], i % 20 == 19 ? '\n' : ' ');
+	printf("\n");
+}
