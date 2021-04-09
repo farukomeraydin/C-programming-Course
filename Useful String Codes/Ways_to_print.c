b41a67cb3c08767c619d4a5f1529d7f94c2f5fc9
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-04-10 00:25:46 +0300

    3 different ways to print strings

---
+++ Useful String Codes/Ways_to_print.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning (disable:6031)
+#include "Omer.h"
+
+int main()
+{
+	char str[] = "Muhittin";
+	puts(str);
+	printf("Alican");
+
+	char name[40] = "Tacettin";
+	char surname[40] = "Eryilmaz";
+	int age = 45;
+	printf("\n%s %s is %d years old\n", name, surname, age);
+}
