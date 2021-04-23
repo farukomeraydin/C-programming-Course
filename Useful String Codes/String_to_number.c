0db1520e574b18ba5f9137293e141424ef6e1a01
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-04-23 12:04:01 +0300

    Entered number as a string becomes integer

---
+++ Useful String Codes/String_to_number.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning (disable:6031)
+#include "Omer.h"
+
+
+#define SIZE 100
+
+int main()
+{
+	char str[SIZE];
+
+	printf("Enter a string: ");
+	scanf("%s", str);
+	printf("[%s]\n", str);
+	int ival = 0;
+
+	for (int i = 0; str[i] != '\0'; ++i)
+		ival = ival * 10 + str[i] - '0';
+	printf("ival = %d\n", ival);
+}
