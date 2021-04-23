c9bc984401fdfdf173a54a5bb05ff803bf878503
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-04-23 11:27:45 +0300

    Erase a character and reprint the entered string

---
+++ Useful String Codes/Erases_character_and_reprints_string.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning (disable:6031)
+#include "Omer.h"
+
+
+#define SIZE 100
+
+int main()
+{
+	char str[SIZE];
+	char buffer[SIZE];
+
+	printf("Enter a string: ");
+	sgets(str);
+	printf("[%s]\n", str);
+
+	int c;
+	printf("Character which will be erased: ");
+	c = getchar();
+
+	int buffer_idx = 0;
+	for (int i = 0; str[i] != '\0'; ++i)
+		if (str[i] != c)
+			buffer[buffer_idx++] = str[i];
+	buffer[buffer_idx] = '\0';
+	printf("[%s]\n", buffer);
+}
