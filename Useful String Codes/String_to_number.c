a85bbc6143e9cc9dc28c27b9dfa0fa53dbd8b2b4
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-04-24 20:53:03 +0300

    String_to_number

--- Useful String Codes/String_to_number.c
+++ Useful String Codes/String_to_number.c
@@ -9,12 +9,22 @@ int main()
 {
 	char str[SIZE];
 
-	printf("Enter a string: ");
+	printf("bir yazi girin: ");
 	scanf("%s", str);
 	printf("[%s]\n", str);
 	int ival = 0;
 
-	for (int i = 0; str[i] != '\0'; ++i)
+	int i = 0, flag = 0;
+
+	if (str[i] == '-')
+		i = 1, flag = 1;
+
+	for (; str[i] != '\0'; ++i) {
 		ival = ival * 10 + str[i] - '0';
+	}
+
+	if (flag)
+		ival = -ival;
+
 	printf("ival = %d\n", ival);
 }
