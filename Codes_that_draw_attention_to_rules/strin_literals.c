4d30605ace332e0c449f016925a6ee60136b3f63
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-07-27 18:16:35 +0300

    Create strin_literals.c

---
+++ Codes_that_draw_attention_to_rules/strin_literals.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+
+int main()
+{
+	printf("%d\n", 3["coders"] - 2["coders"]); //prints 1
+	printf("%d\n", "coders"[3] - "coders"[2]); //prints 1
+}
