4c4020f44db09f33f6e40d844533f2df0066030e
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-07-27 19:53:14 +0300

    Unspecified Behaviour

---
+++ Codes_that_draw_attention_to_rules/Unspecified_Behaviour.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+int main()
+{
+	char *p1 = "Accountability";
+	char *p2 = "Accountability";
+	if (p1 == p2)
+		printf("True\n");
+	else
+		printf("False\n");
+
+	//Unspecified Behaviour
+}
