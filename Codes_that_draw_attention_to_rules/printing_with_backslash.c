b16e73cc15beef57300d57ab98813168b7dc2afa
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-07-27 21:15:09 +0300

    First and second puts is hexa, third one is octa

---
+++ Codes_that_draw_attention_to_rules/printing_with_backslash.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+
+int main()
+{
+	puts("\x42URS\x41"); //BURSA
+	puts("\x42 abadede"); //B abadede
+	puts("\10220\101231\103"); //B20A231C
+
+}
