7ebb59aa00ea203e4f7cf4bcf9bc85d399b645f4
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-08-07 22:30:00 +0300

    Based on where const is you cannot change address

---
+++ Codes_that_draw_attention_to_rules/const_pointer1.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+int a[2] = { 2, 2 };
+int b[3] = { 3, 3, 3 };
+int c[4] = { 4, 4, 4, 4 };
+int d[5] = { 5, 5, 5, 5, 5 };
+
+int main()
+{
+	int *const p[] = { &a, &b, &c, &d };
+
+	int x = 356;
+	//p[2] = &x;	//invalid
+	*p[2] = 666;	//valid
+}
