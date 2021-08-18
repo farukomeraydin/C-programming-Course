41191496b1d2f4dbe4b2414d2202ae23e2dcb74a
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-08-18 16:28:16 +0300

    Using macros for setting and displaying any matrix

---
+++ Multidimensional Arrays/Macro_for_setting_and_displaying_any_matrix.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+gsmf(5)
+gdmf(5)
+gsmf(13)
+gdmf(13)
+gsmf(9)
+gdmf(9)
+
+int main()
+{
+	int a[3][5];
+
+	set_matrix_5(a, 3);
+	display_matrix_5(a, 3);
+}
