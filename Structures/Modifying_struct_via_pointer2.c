d405888ae4b0d123c36e17e61447221c39c2ce76
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-09-09 17:08:12 +0300

    Create Modifying_struct_via_pointer2.c

---
+++ Structures/Modifying_struct_via_pointer2.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+struct Employee {
+	int no_;
+	char name_[20];
+	char surname_[20];
+	double wage_;
+};
+
+int main(void)
+{
+	struct Employee ex = { 2341, "Mustafa", "Yesilkent", 80.40 };
+
+	printf("%5d %16s %16s %.2f\n", ex.no_, ex.name_, ex.surname_, ex.wage_);
+	struct Employee* p = &ex;
+
+	p->no_ = 34;
+	p->wage_ *= 2.3;
+
+	strcat(p->name_, "can");
+	strcat(p->surname_, "li");
+
+	printf("%5d %16s %16s %.2f\n", ex.no_, ex.name_, ex.surname_, ex.wage_);
+}
