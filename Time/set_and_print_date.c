26c5c9d1f7007eaf90757104b52f4bd1dead870b
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-09-11 01:21:11 +0300

    Set and print desired dates

---
+++ Time/set_and_print_date.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+#include "Mytime.h"
+
+int main(void)
+{
+	Date d1;
+	set_date(&d1, 19, 5, 1976);
+	print_date(&d1);
+
+	Date d2;
+	set_date_today(&d2);
+	print_date(&d2);
+
+	Date d3;
+	set_date_str(&d3, "15/05/1992");
+	print_date(&d3);
+}
