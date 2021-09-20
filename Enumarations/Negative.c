a1002eebc4527c5ffad57bd23f84b3b4492f7f47
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-09-20 12:10:44 +0300

    Negative enumarators

---
+++ Enumarations/Negative.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+enum Color {
+	White = -1, Blue, Green = -9, Yellow, Red = 13, Black
+};
+
+int main(void)
+{
+	printf("White = %d\n", White);
+	printf("Blue = %d\n", Blue);
+	printf("Green = %d\n", Green);
+	printf("Yellow = %d\n", Yellow);
+	printf("Red = %d\n", Red);
+	printf("Black = %d\n", Black);
+}
