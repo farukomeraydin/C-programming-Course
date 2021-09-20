33e661510a5bfa387598bc9d62eab7dd254bbcf2
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-09-20 11:00:31 +0300

    Prints enumarations

---
+++ Enumarations/Printing_enums.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+enum Color {
+	White, Blue, Green, Yellow, Red, Black
+};
+
+int main(void)
+{
+	printf("White = %d\n", White);
+	printf("Blue = %d\n", Blue);
+	printf("Green = %d\n", Green);
+	printf("Yellow = %d\n", Yellow);
+	printf("Red = %d\n", Red);
+	printf("Black = %d\n", Black);
+}
