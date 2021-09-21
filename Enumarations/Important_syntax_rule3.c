8c0ce5fbc7ee4ae253c99b8e74edf722af7ade67
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-09-21 10:23:34 +0300

    Create Important_syntax_rule3.c

---
+++ Enumarations/Important_syntax_rule3.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+//Syntax error because Green is redefined
+
+enum TrafficLight{Green, Yellow, Red};
+enum ScreenColor{White, Gray, Green, Magenta, Purple, Black};
+
+int main(void)
+{
+	
+}
