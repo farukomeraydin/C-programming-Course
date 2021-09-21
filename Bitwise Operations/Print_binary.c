9067bf8520ef3b383435d8aa4302bf1401c3b6e6
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-09-21 12:40:33 +0300

    Prints integer as binary

---
+++ Bitwise Operations/Print_binary.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+int main(void)
+{
+	int ival;
+	printf("Enter an integer: ");
+	scanf_s("%d", &ival);
+
+	bprint(ival);
+	bprint(~ival);
+	bprint(~~ival);
+	printf("ival = %d\n", ival);
+}
