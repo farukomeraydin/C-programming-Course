a441297b8a8f6873e57f7e1af3017a6ab8787063
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-11-23 16:03:48 +0300

    It will print 10

---
+++ Command Line Arguments/Reading_new_lines.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+
+int main()
+{
+	FILE* f = fopen("aaaaaa.txt", "r");
+	if (f == NULL) {
+		printf("file cannot be opened\n");
+		return 1;
+	}
+	int c;
+	while ((c = fgetc(f)) != EOF)
+		printf("%d ", c);
+	fclose(f);
+}
