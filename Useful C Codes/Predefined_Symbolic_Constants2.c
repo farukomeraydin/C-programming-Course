718e59eb9aa0e11679a5e61162142f35c2675f15
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-11-30 22:34:41 +0300

    __COUNTER__

---
+++ Useful C Codes/Predefined_Symbolic_Constants2.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+
+int main()
+{
+	printf("%d\n", __COUNTER__);
+	printf("%d\n", __COUNTER__);
+	printf("%d\n", __COUNTER__);
+	printf("%d\n", __COUNTER__);
+}
