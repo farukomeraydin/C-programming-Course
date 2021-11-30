13535d3d78a23345f577569fb8db438b1a9b033d
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-11-30 20:28:50 +0300

    Statement expression

---
+++ Useful C Codes/gcc_extension.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+//With gcc expression statement extension safety problem is solved.
+
+#define maxint(a,b) \
+({int _a = (a), _b = (b); _a > _b ? _a : _b;})
+
+#define gmax(a,b) \
+({typeof(a) _a = (a); \
+  typeof(b) _b = (b); \
+  _a > _b ? _a : _b;})
+
+int main()
+{
+	
+}
