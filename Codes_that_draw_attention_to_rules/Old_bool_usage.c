22a2b1f47cb881ace04fb6f4d615b46b3d5ddcc8
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-12-01 21:25:03 +0300

    Old bool usage

---
+++ Codes_that_draw_attention_to_rules/Old_bool_usage.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+/*Before _Bool type, codes below were tricks for bool*/
+
+//#define Bool	int
+//typedef int Bool;
+//typedef enum {false, true}Bool;
+
+int main()
+{
+	
+}
