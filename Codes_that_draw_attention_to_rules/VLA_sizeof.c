162c3ce6babb5c37b7ec4bf4384a31e3fd79ead7
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-12-01 23:05:15 +0300

    sizeof doesnt produce constant anymore

---
+++ Codes_that_draw_attention_to_rules/VLA_sizeof.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+void func(int x) {
+	int a[x];
+    printf("sizeof(a) = %zu\n", sizeof(a));
+	for (int i = 0; i < x; ++i)
+		a[i] = i;
+	
+	for (int i = 0; i < x; ++i)
+		printf("%d ", a[i]);
+
+	printf("\n");
+}
+
+int main()
+{
+	int ival;
+	printf("Enter an integer: ");
+	scanf("%d", &ival);
+
+	func(ival);
+}
