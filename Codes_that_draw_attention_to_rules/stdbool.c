3f8b04da5829ffa53654dd2b3e2a7e227bc08d42
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-12-01 21:37:35 +0300

    Macros inside stdbool.h

---
+++ Codes_that_draw_attention_to_rules/stdbool.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+#include <stdbool.h>
+
+int main()
+{
+	//bool becomes _Bool, false becomes 0, true becomes 1
+	bool flag = false;
+
+	flag = true;
+}
