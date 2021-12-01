65b90147003a577d46628e71ec47ec6e89442cdf
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-12-01 21:16:22 +0300

    Declaration difference between C89 and C99

---
+++ Codes_that_draw_attention_to_rules/C89_valid_declaration.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Omer.h"
+
+int main()
+{
+	/*printf("Alican ");
+	int x = 20; //Invalid in C89*/
+
+	/*int x = 20;
+	printf("Marcus ");*/
+
+	//is valid or 
+
+	/*
+	printf("Marcus "); {
+		int x = 20;
+	}
+	*/
+	//is valid in C89
+	
+}
